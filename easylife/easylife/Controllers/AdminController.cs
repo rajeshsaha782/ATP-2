948386625201c219f42e220748ebe4f3a3448b38
Rajesh saha <33521669+rajeshsaha782@users.noreply.github.com>
2018-04-07 21:38:27 +0600

    admin home setup

--- easylife/easylife/Controllers/AdminController.cs
+++ easylife/easylife/Controllers/AdminController.cs
@@ -13,8 +13,68 @@ namespace easylife.Controllers
         {
             return View();
         }
+        public ActionResult Edit_Profile_Admin()
+        {
+            return View();
+        }
+        public ActionResult View_Profile_Admin()
+        {
+            return View();
+        }
+        public ActionResult Change_Password()
+        {
+            return View();
+        }
 
+        //---------------------------------------------
+
+        public ActionResult Add_Member()
+        {
+            return View();
+        }
+        public ActionResult Add_Product()
+        {
+            return View();
+        }
+
+        //---------------------------------------------
+
+        public ActionResult View_Members()
+        {
+            return View();
+        }
+        public ActionResult View_Delivery_Man()
+        {
+            return View();
+        }
+        public ActionResult View_Products()
+        {
+            return View();
+        }
+        public ActionResult View_Users()
+        {
+            return View();
+        }
+        public ActionResult View_Invoices()
+        {
+            return View();
+        }
+        public ActionResult View_Orders()
+        {
+            return View();
+        }
+        public ActionResult View_Stocks()
+        {
+            return View();
+        }
         public ActionResult Profit_graph()
+        {
+            return View();
+        }
+
+        //---------------------------------------------
+
+        public ActionResult View_Invoice()
         {
             return View();
         }
