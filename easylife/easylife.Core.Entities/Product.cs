bccef58bfada50ce3afb809d6d94a2f511849a65
Rajesh saha <33521669+rajeshsaha782@users.noreply.github.com>
2018-04-27 15:33:24 +0600

    csca

---
+++ easylife/easylife.Core.Entities/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace easylife.Core.Entities
+{
+    public class Product
+    {
+        public int Product_id { get; set; }
+        public string Product_name { get; set; }
+        public string Features { get; set; }
+        public float Quantity { get; set; }
+        public float Buying_Price { get; set; }
+        public float Selling_Price { get; set; }
+        public string Category { get; set; }
+        public string SubCategory { get; set; }
+       
+    }
+}
