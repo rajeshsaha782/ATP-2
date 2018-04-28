7ba227ec5f33d1e8351612bda7d03a06316c4e5a
Rajesh saha <33521669+rajeshsaha782@users.noreply.github.com>
2018-04-28 19:45:09 +0600

    test add

---
+++ easylife/Test/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using easylife.Infrastructure;
+using easylife.Core.Entities;
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ///---------email Test
+
+            //SendEmail email = new SendEmail();
+
+            //if(email.Email("rajeshsaha782@gmail.com", "test", "<h1>Success</h1>"))
+            //{
+            //    Console.WriteLine("Send");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Error");
+            //}
+
+
+            ///----------EF Test
+
+            easylifeDbContext e = new easylifeDbContext();
+            Member m = new Member();
+            m.Email = "r@gmail.com";
+            m.Name = "rajesh";
+
+            e.Members.Add(m);
+            e.SaveChanges();
+
+        }
+    }
+}
