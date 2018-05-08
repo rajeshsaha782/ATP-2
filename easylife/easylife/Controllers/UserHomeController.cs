76a99b257d58bf0449e783b96e9f20d8babf0b7f
Rajesh saha <33521669+rajeshsaha782@users.noreply.github.com>
2018-05-08 23:07:33 +0600

    track product error fix

--- easylife/easylife/Controllers/UserHomeController.cs
+++ easylife/easylife/Controllers/UserHomeController.cs
@@ -467,17 +467,22 @@ namespace easylife.Controllers
 
             string Status, Payment, msg;
 
-            Status = _InvoiceService.GetById(id).Status;
-            Payment = _InvoiceService.GetById(id).PaymentStatus;
+            
 
             if(Session["userId"] == null)
             {
                 return "Not_logged";
             }
-            if (_InvoiceService.GetById(id).MemberId != Convert.ToInt32(Session["userId"]))
+            else if (_InvoiceService.GetById(id).MemberId != Convert.ToInt32(Session["userId"]))
             {
                 return "Invaild Invoice number.Enter for a valid Invoice";
             }
+            
+
+
+
+            Status = _InvoiceService.GetById(id).Status;
+            Payment = _InvoiceService.GetById(id).PaymentStatus;
 
             if (Status == "0")
             {
